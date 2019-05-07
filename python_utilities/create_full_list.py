c78e47a0c3cadf0c09a8ee4a42b4dd606c87ce7f
mxrn <mxrn@no-reply.de>
2019-05-08 00:40:02 +0200

    i#11: first try on fixing

--- python_utilities/create_full_list.py
+++ python_utilities/create_full_list.py
@@ -5,17 +5,14 @@ full_list = []
 
 for fileobj in os.scandir(directory):
     if fileobj.path.endswith(".csv"):
-        if fileobj.path.endswith("_full_list.csv"):
-            pass
-        else:
-            with open(fileobj.path,'r') as entries:
-                for entry in entries:
-                    if entry not in full_list:
-                        if entry.endswith("\n"):
-                            full_list.append(entry)
-                        else:
-                            print(entry+"\n")
-                            full_list.append(entry+"\n")
+        with open(fileobj.path,'r') as entries:
+            for entry in entries:
+                if entry not in full_list:
+                    if entry.endswith("\n"):
+                        full_list.append(entry)
+                    else:
+                        print(entry+"\n")
+                        full_list.append(entry+"\n")
 
 with open(directory+"_full_list.csv", mode = "w+", encoding = "utf-8") as fobj:
     for entry in set(full_list):
